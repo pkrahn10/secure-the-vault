3637b6aac814aee2762208db3ffcf306a95300bb
Parker <parkerkrahn10@gmail.com>
2025-03-03 19:23:37 -0800

    updated file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # secure-the-vault
+
+Assignment
---
+++ script.js
@@ -0,0 +1,20 @@
+// # Step 1: Display message about the vault combonation
+
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination"; //message telling you that you have been chosen to open vault
+
+// # Step 2: 7 + 3 = num1
+
+const num1 = 7 + 3; //10
+
+// # Step 3: 4 * 10 = num2
+
+const num2 = 4 * 10; //40
+
+// # Step 4: 45 - 6 = num3
+
+const num3 = 45 - 6; //39
+
+// # Step 5: Create alert to display message with combonation
+
+alert(`${message}: ${num1}-${num2}-${num3}`); //alert displaying combonation
